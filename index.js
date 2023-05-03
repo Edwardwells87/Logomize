473fbb4172847a4ecb4cd5967da116a8d59b95bb
Edward Wells <edward.wells87@gmail.com>
2023-05-02 20:10:27 -0500

    changing the way most class functions are wrote, they arent working together well

--- index.js
+++ index.js
@@ -1,32 +1,31 @@
 const inquirer = require('inquirer');
-const fs = require('fs');
+//const fs = require('fs');
 const ShapeMaker = require('./lib/shapes');
-const initialsLengthResult = require("./lib/initials");
-const colorsResult = require("./lib/colors")
+const InitialsLength = require("./lib/initials");
+const Colors = require("./lib/colors")
 
-class LogoMaker {
-  constructor() {
-    this.answers = inquirer.prompt([
+function LogoMaker() {
+  inquirer
+    .prompt([
       {
         type: 'input',
         message: 'Please provide up to 3 letters that represent your company:',
         name: 'initials'
       },
+
       {
         type: 'list',
-        message: 'Select a shape:',
         name: 'shapes',
-        choices: [
-          'circle',
-          'square',
-          'triangle',
-        ]
+        message: 'Select a shape',
+        choices: ['circle', 'square', 'triangle']
       },
+
       {
         type: 'input',
         message: "Please enter a hexadecimal code for the shape's color here:",
         name: 'color'
       },
+
       {
         type: 'input',
         message: "Please enter a hexadecimals code for the letter's color here:",
@@ -35,21 +34,22 @@ class LogoMaker {
     ])
     .then((answers) => {
       console.log('wtf is going on---------')
-      return Promise.all([
-        InitialsLength.check(answers),
-        ShapeMaker.check(answers),
-        Colors.check(answers)
+       return Promise.all([
+        new InitialsLength(answers), 
+       new Colors(answers),
+       new ShapeMaker(answers),
       ])
-    })
-    .then((shapeMakerResult) => {
-      // All functions have completed successfully, so write the file shouldnt be hard
-      const svgCode = shapeMakerResult;
-      console.log('this far-----------');
-      fs.writeFile('logo.svg', svgCode, (err) => {
-        if (err) throw err('please try again');
-        console.log('Logo file has been generated in the root folder');
-      });
-    });
-  }
+})
+   // }
+  // .then((shapeMakerResult) => {
+  // All functions have completed successfully, so write the file shouldnt be hard
+  //   const svgCode = shapeMakerResult;
+  //  console.log('this far-----------');
+  // fs.writeFile('logo.svg', svgCode, (err) => {
+  //  if (err) throw err('please try again');
+  // console.log('Logo file has been generated in the root folder');
+  // });
+  //});
 }
-const logoMaker = new LogoMaker()
+
+LogoMaker()
